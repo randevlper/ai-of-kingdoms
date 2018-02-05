7582389054434a138b8f9da35b67efb59e25fe20
randevpler <randallhines0@gmail.com>
2018-02-05 12:20:09 -0800

    Added interfaces.

---
+++ Assets/Randall/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IDamageable
+{
+    void Damage(float damage);
+}
+
+interface IHealable
+{
+    void Heal(float heal);
+}
+
+interface IStorage
+{
+    void Insert(int num);
+    void Take(int num);
+}
