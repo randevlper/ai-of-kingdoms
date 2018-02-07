4104e829c2ac5ed69b9a19c295b2998ee6e09598
randevpler <randallhines0@gmail.com>
2018-02-06 17:08:50 -0800

    Some node changes

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -2,19 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour {
+public class Node : MonoBehaviour
+{
+    public float detectionDistance;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	//If AI in sphere overlap start capture
-	//If two or more pause capture
-	//Once captured heal AI
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Detect()
+    {
+        Collider[] hits =
+    Physics.OverlapSphere(transform.position, detectionDistance);
+
+        for (int i = 0; i < hits.Length; ++i)
+        {
+            if (hits[i].tag != tag)
+            {
+                IDamageable enemy = hits[i].GetComponent<IDamageable>();
+                if (enemy != null)
+                {
+
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, detectionDistance);
+    }
+
+    //If AI in sphere overlap start capture
+    //If two or more pause capture
+    //Once captured heal AI
 }
