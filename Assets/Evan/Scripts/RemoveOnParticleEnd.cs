2f0f7004e302dad96a41b2f3034acac935e13d1b
Evan Knebel <s179060@students.aie.edu.au>
2018-02-12 16:35:05 -0800

    Explosions!

---
+++ Assets/Evan/Scripts/RemoveOnParticleEnd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveOnParticleEnd : MonoBehaviour
+{
+    ParticleSystem ps;
+	// Use this for initialization
+	void Start ()
+    {
+        ps = GetComponentInChildren<ParticleSystem>();
+        ps.loop = false;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(ps.isStopped)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+}
